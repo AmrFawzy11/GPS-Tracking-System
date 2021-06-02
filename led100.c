04053a6a4424aee013263f3d587eef54ffe3c403
AmrFawzy11 <84874689+AmrFawzy11@users.noreply.github.com>
2021-06-03 01:23:58 +0200

    Update led100.c

--- led100.c
+++ led100.c
@@ -1,6 +1,7 @@
+// function turns on the LED when the distance exceeds 100 meters.
 void led_out(unsigned char data)
 {
-if( data>=0x64)
-{GPIO_PORTF_DATA_R |= 0x02}
+if( data>=0x64) // checking if distance covered exceeded 100 meter
+{GPIO_PORTF_DATA_R |= 0x02} // turn on red led
 	
 	}
