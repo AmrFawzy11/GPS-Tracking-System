d4ab757689531ea8843370f94d481fbb3f9774ee
amirsaleh55 <85321216+amirsaleh55@users.noreply.github.com>
2021-06-06 22:00:23 +0200

    Update output.c

--- output.c
+++ output.c
@@ -1,4 +1,37 @@
+#include "C:/Keil/ARM/INC/ARM/tm4c123gh6pm.h"
+#include "stdint.h"
+void print(int mod);
+void delay(int x);
+
 // print distance covered on port B
 void LCD_OUT(unsigned char distance){
-	GPIO_PORTB_DATA_R = distance;  // write output distance on port B
+	
+	 int mod1, mod2, mod3;
+	
+    mod1 = distance %10;
+	  distance /=10;
+	
+	  mod2 = distance %10;
+	  distance /=10;
+	
+	  mod3 = distance %10;
+	  distance /=10;
+	
+	 
+		
+		GPIO_PORTA_DATA_R = ~0x08;
+		print(mod1);
+	  delay(1000);
+		
+		GPIO_PORTA_DATA_R = ~ 0x04;
+		print(mod2);
+	  delay(1000);
+		GPIO_PORTA_DATA_R = ~ 0x80;
+
+		print(mod3);
+	  delay(1000);
+	
+	
+	
+	
 }
